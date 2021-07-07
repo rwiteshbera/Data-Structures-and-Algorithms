03168134cdfa5800dcb90bc3c2f1a4753ef14d18
rwiteshbera <rwiteshbera@gmail.com>
2021-07-07 22:34:01 +0530

    Updated Hashing

--- src/rwitesh/hashing/IntersectionOfTwoArrays.java
+++ src/rwitesh/hashing/IntersectionOfTwoArrays.java
@@ -6,44 +6,23 @@ Input : arr1[] = {1, 3, 4, 5, 7}
 Output : Intersection = {3,5}
 */
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class IntersectionOfTwoArrays {
-    //Iterative Approach
-    static void Intersection1(int[] arr1, int[] arr2)
-    {
-        ArrayList<Integer>Temp = new ArrayList<>();
-        int i =0 ,j = 0;
-        while (i<arr1.length && j<arr2.length)
+    //Iterative Approach // Time Complexity: O(M*N) If
+    // the size of the array 1 is M and array 2 is N
+    static void Intersection1(int[] arr1, int[] arr2){
+        for(int i=0; i<arr1.length; i++)
         {
-            if(arr1[i]<arr2[j])
+            for(int j=0; j<arr2.length; j++)
             {
-                Temp.add(arr1[i++]);
-            }
-            else if(arr1[i]>arr2[j]){
-                Temp.add(arr2[j++]);
-            }
-            else {
-                Temp.add(arr1[i]);
-                i++;
-                j++;
+                if (arr1[i]==arr2[j]){
+                    System.out.print(arr1[i] + " ");
+                    break;
+                }
             }
         }
-
-        //Handling remaing element of the larger array
-        while (i<arr1.length){
-            Temp.add(arr1[i++]);
-        }
-        while (j<arr2.length){
-            Temp.add(arr2[j++]);
-        }
-
-        for (Integer integer : Temp) {
-            System.out.print(integer + " ");
-        }
-        System.out.println();
     }
 
 
@@ -65,7 +44,7 @@ public class IntersectionOfTwoArrays {
         int[] arr1 = {1,3,4,5,7};
         int[] arr2 = {2,3,5,6};
 
-//        Intersection1(arr1,arr2);
-        Intersection2(arr1,arr2);
+        Intersection1(arr1,arr2);
+//        Intersection2(arr1,arr2);
     }
 }
